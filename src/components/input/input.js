a0593bfac40d4e76deec5eae2bc55db6dc0b6eed
homePC <vladimirandrosov13@gmail.com>
2023-04-06 13:59:02 +0300

    refactor

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,6 +1,6 @@
 import inputTpl from "./input.hbs";
 import "./input.css";
 
-export const inputTemplate = (inputType, overText) => {
-    return inputTpl({inputType, overText});
+export const inputTemplate = (inputType, overText, name) => {
+    return inputTpl({inputType, overText, name});
 }
