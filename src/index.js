1d44282aef578938c710008c1bc773a70b3cdddb
homePC <vladimirandrosov13@gmail.com>
2023-04-08 00:03:54 +0300

    добавил исправления: сделал ссылки списком, поместил их в тег nav, кнопки обернул в тег button и поместил в form, добавил css переменные к проекту, поместил контент в тег main

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import templateButton from "./components/button/index.js";
 import chatPage from "./pages/chats/index.js";
 
 
-const main = document.querySelector('.main');
-
-
+const root = document.querySelector('.root');
 
 const getLoginPage = (event) => {
     event.preventDefault();
-    main.innerHTML = loginPage();
+    root.innerHTML = loginPage();
     document.querySelector('.login__form-nickname').innerHTML = inputTemplate("text", "Логин", 'login');
     document.querySelector('.login__form-password').innerHTML = inputTemplate("password", "Пароль", 'password');
     document.querySelector('.login__btn').innerHTML = templateButton("Авторизоваться");
@@ -22,7 +20,7 @@ const getLoginPage = (event) => {
 
 const getRegistrationPage = (event) => {
     event.preventDefault();
-    main.innerHTML = registrationPage();
+    root.innerHTML = registrationPage();
     document.querySelector('.registration__form-email').innerHTML = inputTemplate("text", "Почта", 'email');
     document.querySelector('.registration__form-login').innerHTML = inputTemplate("text", "Логин", 'login');
     document.querySelector('.registration__form-name').innerHTML = inputTemplate("text", "Имя", 'first_name');
@@ -46,21 +44,21 @@ document.querySelector('#registration').addEventListener('click', (event) => {
 
 document.querySelector('#user-settings').addEventListener('click', (event) => {
     event.preventDefault();
-    main.innerHTML = userSettingsPage();
+    root.innerHTML = userSettingsPage();
     document.querySelector('.user-settings__btn').innerHTML = templateButton("Сохранить");
 });
 
 document.querySelector('#server-error').addEventListener('click', (event) => {
     event.preventDefault();
-    main.innerHTML = serverErrorTemplate('505', 'Мы уже фиксим');
+    root.innerHTML = serverErrorTemplate('505', 'Мы уже фиксим');
 });
 
 document.querySelector('#user-error').addEventListener('click', (event) => {
     event.preventDefault();
-    main.innerHTML = userErrorTemplate('404', 'Упс, не туда');
+    root.innerHTML = userErrorTemplate('404', 'Упс, не туда');
 });
 
 document.querySelector('#chats').addEventListener('click', (event) => {
     event.preventDefault();
-    main.innerHTML = chatPage();
+    root.innerHTML = chatPage();
 })
